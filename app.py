6e65565697e2d90f581c0ffc93d753816e423070
Francisco de Assis Boldt <fboldt@gmail.com>
2022-10-08 10:40:05 -0300

    fix minor bug

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def post_something():
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
+            "Message": f"Welcome to our awesome platform!!",
             # Add this option to distinct the POST request
             "METHOD" : "POST"
         })
