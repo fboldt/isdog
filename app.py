0c810911534ad69c7c53683a56ba99b9398ea654
mituskury <arturcarvalho05@gmail.com>
2022-10-18 09:56:28 -0300

    commit adjustment

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 from flask import Flask, current_app
+import requests
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return current_app.send_static_file('index.html')
 
-if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+#rota da pagina principal
+
+@app.route('/')
+def homepage():
+    return current_app.send_static_file('home.html')
+
+#rotas alternativas 
+
+@app.route("/app")
+def aplication():
+    return current_app.send_static_file("rota1.html")
+
+@app.route("/app/imagem") #rota a definir no futuro
+def resposta():
+    return current_app.send_static_file("rota2.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=8081, debug = True)
+
+
+
+#utilizar post pra enviar uma imagem pro site esperando uma resposta de true ou false
+#exemplo request.post(link, data=imagem)
