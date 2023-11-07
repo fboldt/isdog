a326048e157842d690d526c733d78526634593ba
Francisco de Assis Boldt <fboldt@gmail.com>
2023-11-07 14:55:41 -0300

    update incomplete assessall.py

--- assessall.py
+++ assessall.py
@@ -1,7 +1,6 @@
 from assessarchiteture import assessarchiteture
-import tensorflow as tf
-from tf.keras.applications.xception import Xception
-from tf.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.xception import Xception
+from tensorflow.keras.applications.vgg16 import VGG16
 
 architetures = [
     Xception(input_shape = [image_size,image_size,3], weights='imagenet', include_top=False),
