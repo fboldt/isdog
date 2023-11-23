f11cd5ba58f3f7538cb571663b2b3487c198f827
Francisco de Assis Boldt <fboldt@gmail.com>
2023-11-23 13:24:30 -0300

    framework to assess keras architectures

--- assessAll.py
+++ assessAll.py
@@ -1,8 +1,15 @@
 from assessArchitecture import assessArchitecture
-from tensorflow.keras.applications.xception import Xception
-from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow import keras
+import numpy as np
 
-architectures = [VGG16, Xception]
+# https://keras.io/api/applications/
+architectures = [
+    ("Xception", keras.applications.xception.Xception, keras.applications.xception.preprocess_input),
+    ("VGG16", keras.applications.vgg16.VGG16, keras.applications.vgg16.preprocess_input), 
+    ("VGG19", keras.applications.vgg19.VGG19, keras.applications.vgg19.preprocess_input), 
+    ]
 
-for architecture in architectures:
-    print(assessArchitecture(architecture))
+for checkpoint, architecture, preprocess_input in architectures:
+    scores, history = assessArchitecture(architecture, preprocess_input, checkpoint+".best")
+    np.savetxt("scores/"+checkpoint+".csv", scores)
+    print(scores)
--- assessArchitecture.py
+++ assessArchitecture.py
@@ -19,7 +19,13 @@ def make_subset(subset="training", directory="kaggle"):
 train_dataset = make_subset("training")
 validation_dataset = make_subset("validation")
 
-def assessArchitecture(architecture, checkpoint="architecture.dbg"):
+def fineTuningLayers(x):
+    x = layers.BatchNormalization()(x)
+    x = layers.GlobalMaxPooling2D()(x)
+    x = layers.Dropout(0.5)(x)
+    return x
+
+def assessArchitecture(architecture, preprocess_input, checkpoint="architecture.dbg", debug=False):
     num_labels = len(train_dataset.class_names)
     
     conv_base = architecture(weights="imagenet", include_top=False)
@@ -29,12 +35,11 @@ def assessArchitecture(architecture, checkpoint="architecture.dbg"):
                                 train_dataset.element_spec[0].shape[2], 
                                 train_dataset.element_spec[0].shape[3]))
     x = data_augmentation(inputs)
-    x = keras.applications.vgg16.preprocess_input(x)
+    if preprocess_input:
+        x = preprocess_input(x)
     x = conv_base(x)
 
-    x = layers.BatchNormalization()(x)
-    x = layers.GlobalMaxPooling2D()(x)
-    x = layers.Dropout(0.5)(x)
+    x = fineTuningLayers(x)
     outputs = layers.Dense(num_labels, activation='softmax')(x)
     model = keras.Model(inputs, outputs)
 
@@ -44,8 +49,7 @@ def assessArchitecture(architecture, checkpoint="architecture.dbg"):
                                                  monitor="val_loss"), 
                  keras.callbacks.EarlyStopping(monitor="val_loss", 
                                                patience=5) ]
-    history = model.fit(train_dataset, epochs=30, validation_data=validation_dataset, callbacks=callbacks)
+    epochs = 1 if debug else 50
+    history = model.fit(train_dataset, epochs=epochs, validation_data=validation_dataset, callbacks=callbacks)
     model = keras.models.load_model(checkpoint)
     return model.evaluate(validation_dataset), history
-
-# evaluation, history = assessArchitecture(keras.applications.vgg16.VGG16)
